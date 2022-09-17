d6f443c9c89914b5fd9871c3e8650e5387584ffd
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-09-17 12:16:36 -0700

    Seperated register route to it's own file

--- server/app.js
+++ server/app.js
@@ -2,10 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser'); // access post data from client
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');	// authorize users
-var bcrypt = require('bcrypt');	// hashes pass saved to database and verifies login token
 var cors = require('cors');
 var dotenv = require('dotenv');
-var User = require('./models/user'); // user schema
 dotenv.config({path: './.env'});
 
 var app = express();
@@ -13,6 +11,7 @@ var app = express();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json(), urlencodedParser);
 app.use(cors());
+app.use('/register', require('./routes/register'));
 
 const dbURI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.rb2mrgc.mongodb.net/?retryWrites=true&w=majority`
 
---
+++ server/routes/register.js
@@ -0,0 +1,30 @@
+var router = require('express').Router();
+var bcrypt = require('bcrypt');	// hashes pass saved to database and verifies login token
+var User = require('../models/user'); // user schema
+
+// Route for registering a new user
+app.post('/', async (req, res) => {
+	const user = req.body;
+
+	// Verify user and email aren't already taken
+	const takenUser = await User.findOne({ username: user.username });
+	const takenEmail = await User.findOne({ email: user.email });
+
+	if (takenUser || takenEmail) {
+		res.json({ message: "User/Email already taken "});
+	} else {
+		// (1024 rounds of hashing) increase for security, decrease for performance
+		user.password = await bcrypt.hash(req.body.password, 10);
+		
+		const dbUser = new User({
+			username: user.username.toLowerCase(),
+			email: user.email.toLowerCase(),
+			password: user.password
+		});
+
+		dbUser.save();
+		res.json({ message: "Success" });
+	}
+});
+
+module.exports = router;
