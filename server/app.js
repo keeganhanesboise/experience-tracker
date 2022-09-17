18be726a03a3958c1eaad3a0fb964b8d737ece9d
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-09-17 10:33:23 -0700

    Set up client and server

---
+++ server/app.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+	res.send('Experience Tracker');
+});
+
+app.listen(5000, function () {
+	console.log('listening on port 5000');
+});
