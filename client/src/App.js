583ca9807d9df8ffdccdac4156ce80ac17362515
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-09-17 11:15:16 -0700

    Connected client to server

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get('http://localhost:5000', {})
+    .then(res => setData(res.data))
+    .catch(err => console.log(err));    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {data}
     </div>
   );
 }
