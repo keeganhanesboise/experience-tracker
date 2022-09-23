9b038c842f2b1a3bc47f30ed3d564cd36d687921
keeganhanesboise <70166916+keeganhanesboise@users.noreply.github.com>
2022-09-23 10:51:42 -0700

    Created Image Modal Component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar";
 import ExperienceForm from "./components/experienceForm";
 import ExperienceDisplay from "./components/experienceDisplay";
 import CollectionForm from "./components/collectionForm";
+import ImageModal from "./components/imageModal";
 import "./App.css";
 
 function App() {
@@ -392,38 +393,7 @@ function App() {
               </div>
             </div>
           </div>
-          <div
-            className="modal fade"
-            id="exampleModal"
-            tabIndex="-1"
-            aria-labelledby="exampleModalLabel"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog">
-              <div className="modal-content">
-                <div class="modal-header">
-                  <h5 class="modal-title" id="exampleModalLabel">{displayName}</h5>
-                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div className="modal-body">
-                  <img
-                    src={displayImage}
-                    alt="experience"
-                    style={{ width: "100%", height: "auto" }}
-                  ></img>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-bs-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ImageModal displayImage={displayImage} displayName={displayName} />
         </div>
       ) : (
         <div className="container-fluid" style={{ maxWidth: "500px" }}>
---
+++ client/src/components/imageModal.js
@@ -0,0 +1,38 @@
+function ImageModal(props) {
+    return (
+        <div
+            className="modal fade"
+            id="exampleModal"
+            tabIndex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+        >
+            <div className="modal-dialog">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">{props.displayName}</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <img
+                            src={props.displayImage}
+                            alt="experience"
+                            style={{ width: "100%", height: "auto" }}
+                        ></img>
+                    </div>
+                    <div className="modal-footer">
+                        <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ImageModal;
